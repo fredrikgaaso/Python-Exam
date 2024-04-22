67f6844b8aa2646b55637fd0891677c91e213dac
felixbyrjall <felixbyrjall@gmail.com>
2024-04-22 15:46:24 +0200

    Added a const LIST_LENGTH to ensure that the range in substitute() is as long as the list randomNumber

--- Questions/Question4.py
+++ Questions/Question4.py
@@ -1,14 +1,16 @@
 import random
 
+LIST_LENGTH = 10
+
 
 def main():
-    randomNumber = [random.randint(1, 50) for i in range(10)]
+    randomNumber = [random.randint(1, 50) for i in range(LIST_LENGTH)]
     print(randomNumber)
 
     def substitute():
-        for i in range(10):
+        for i in range(LIST_LENGTH):
             if randomNumber[i] % 5 == 0:
-                var = randomNumber[i] * randomNumber[i]
+                var = randomNumber[i] ** 2
                 randomNumber[i] = var
         print(randomNumber)
 
