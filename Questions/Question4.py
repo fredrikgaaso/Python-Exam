c355f125122fbcad9f7bc4699b17cd3ffcf7320a
fredrikgaaso <fredrikravndalgaaso@gmail.com>
2024-04-22 12:57:51 +0200

    q4

--- Questions/Question4.py
+++ Questions/Question4.py
@@ -4,11 +4,15 @@ import random
 def main():
     randomNumber = [random.randint(1, 50) for i in range(10)]
     print(randomNumber)
-    for i in range(10):
-        if randomNumber[i] % 5 == 0:
-            var = randomNumber[i] * randomNumber[i]
-            randomNumber[i] = var
-    print(randomNumber)
+
+    def substitute():
+        for i in range(10):
+            if randomNumber[i] % 5 == 0:
+                var = randomNumber[i] * randomNumber[i]
+                randomNumber[i] = var
+        print(randomNumber)
+
+    substitute()
 
 
 main()
