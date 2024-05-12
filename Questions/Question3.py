8643e5ca9a2cde30791a48293fb3aaee23aaf051
felixbyrjall <felixbyrjall@gmail.com>
2024-05-12 15:21:45 +0200

    Fixed q3

--- Questions/Question3.py
+++ Questions/Question3.py
@@ -1,27 +1,27 @@
-def main() :
-
-    class Message():
+def main():
+    class Message:
 
         def __init__(self, sender, recipient):
-            self._sname = sender
-            self._rname = recipient
+            self._senderName = sender
+            self._recipientName = recipient
             self._message = ""
 
         def get_sender(self):
-            return self._sname
+            return self._senderName
 
         def get_recipient(self):
-            return self._rname
+            return self._recipientName
 
         def append(self, message):
             self._message += message
 
         def toString(self):
-            print("From: " + self.get_sender() + "\nTo: " + self.get_recipient() + "\nMessage: " + self._message)
+            print("From:", self.get_sender(), "\nTo:", self.get_recipient(), "\nMessage:", self._message)
 
-    new_message = Message("Me", "You")
+    new_message = Message("Harry Morgan", "Rudolf Reindeer")
     new_message.append("Hello! ")
     new_message.append("How are you?")
     new_message.toString()
+
 
 main()
