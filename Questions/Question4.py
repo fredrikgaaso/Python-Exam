ab984adede3a2c8b535e81963d88b6ee626a5155
felixbyrjall <felixbyrjall@gmail.com>
2024-05-12 15:35:00 +0200

    Fixed q4

--- Questions/Question4.py
+++ Questions/Question4.py
@@ -3,20 +3,22 @@ import random
 LIST_LENGTH = 10
 START = 1
 END = 50
+MULTIPLE = 5
+
+
+def substitute(random_number):
+    for i in range(LIST_LENGTH):
+        if random_number[i] % MULTIPLE == 0:
+            squared = random_number[i] ** 2
+            random_number[i] = squared
+    return random_number
 
 
 def main():
-    randomNumber = [random.randint(START, END) for i in range(LIST_LENGTH)]
-    print(randomNumber)
+    list_of_numbers = [random.randint(START, END) for i in range(LIST_LENGTH)]
 
-    def substitute():
-        for i in range(LIST_LENGTH):
-            if randomNumber[i] % 5 == 0:
-                var = randomNumber[i] ** 2
-                randomNumber[i] = var
-        print(randomNumber)
-
-    substitute()
+    print("Before substitution, the list is:", list_of_numbers)
+    print("After substitution, the list is:", substitute(list_of_numbers))
 
 
 main()
